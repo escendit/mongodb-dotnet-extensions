511c35f8934e7453d1e201f2162bd612de067eb0
Suhe <sdkaraarslan@gmail.com>
2023-08-24 22:29:21 +0200

    chore: add named registration for mongodb options

---
+++ src/Client/HostBuilderExtensions.cs
@@ -0,0 +1,139 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Clients.MongoDb
+{
+    using System.Diagnostics.CodeAnalysis;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Options;
+
+    /// <summary>
+    /// Host Builder Extensions.
+    /// </summary>
+    [DynamicallyAccessedMembers(
+        DynamicallyAccessedMemberTypes.All)]
+    public static class HostBuilderExtensions
+    {
+        /// <summary>
+        /// Add MongoDb Client Options As Default.
+        /// </summary>
+        /// <param name="hostBuilder">The host builder.</param>
+        /// <param name="configureOptions">The configure options.</param>
+        /// <returns>The updated host builder.</returns>
+        public static IHostBuilder AddMongoDbClientOptionsAsDefault(
+            this IHostBuilder hostBuilder,
+            Action<MongoDbOptions> configureOptions)
+        {
+            ArgumentNullException.ThrowIfNull(hostBuilder);
+            ArgumentNullException.ThrowIfNull(configureOptions);
+            return hostBuilder
+                .ConfigureServices(services => services
+                    .AddOptions<MongoDbOptions>(MongoDbOptions.DefaultKey)
+                    .Configure(configureOptions));
+        }
+
+        /// <summary>
+        /// Add MongoDb Client Options As Default.
+        /// </summary>
+        /// <param name="hostBuilder">The initial host builder.</param>
+        /// <param name="configureOptions">The configure options.</param>
+        /// <returns>The updated host builder.</returns>
+        public static IHostBuilder AddMongoDbClientOptionsAsDefault(
+            this IHostBuilder hostBuilder,
+            Action<OptionsBuilder<MongoDbOptions>> configureOptions)
+        {
+            ArgumentNullException.ThrowIfNull(hostBuilder);
+            ArgumentNullException.ThrowIfNull(configureOptions);
+            return hostBuilder
+                .ConfigureServices(services =>
+                {
+                    configureOptions.Invoke(services
+                        .AddOptions<MongoDbOptions>(MongoDbOptions.DefaultKey));
+                });
+        }
+
+        /// <summary>
+        /// Add MongoDb Client Options As Default.
+        /// </summary>
+        /// <param name="hostBuilder">The initial host builder.</param>
+        /// <param name="configSectionPath">The config section path.</param>
+        /// <returns>The updated host builder.</returns>
+        public static IHostBuilder AddMongoDbClientOptionsAsDefault(
+            this IHostBuilder hostBuilder,
+            string configSectionPath)
+        {
+            ArgumentNullException.ThrowIfNull(hostBuilder);
+            ArgumentNullException.ThrowIfNull(configSectionPath);
+            return hostBuilder
+                .ConfigureServices(services => services
+                    .AddOptions<MongoDbOptions>(MongoDbOptions.DefaultKey)
+                    .BindConfiguration(configSectionPath));
+        }
+
+        /// <summary>
+        /// Add MongoDb Client Options.
+        /// </summary>
+        /// <param name="hostBuilder">The initial host builder.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="configureOptions">The configure options.</param>
+        /// <returns>The updated host builder.</returns>
+        public static IHostBuilder AddMongoDbClientOptions(
+            this IHostBuilder hostBuilder,
+            string name,
+            Action<MongoDbOptions> configureOptions)
+        {
+            ArgumentNullException.ThrowIfNull(hostBuilder);
+            ArgumentNullException.ThrowIfNull(name);
+            ArgumentNullException.ThrowIfNull(configureOptions);
+            return hostBuilder
+                .ConfigureServices(services => services
+                    .AddOptions<MongoDbOptions>(name)
+                    .Configure(configureOptions));
+        }
+
+        /// <summary>
+        /// Add MongoDb Client Options.
+        /// </summary>
+        /// <param name="hostBuilder">The initial host builder.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="configureOptions">The configure options.</param>
+        /// <returns>The updated host builder.</returns>
+        public static IHostBuilder AddMongoDbClientOptions(
+            this IHostBuilder hostBuilder,
+            string name,
+            Action<OptionsBuilder<MongoDbOptions>> configureOptions)
+        {
+            ArgumentNullException.ThrowIfNull(hostBuilder);
+            ArgumentNullException.ThrowIfNull(name);
+            ArgumentNullException.ThrowIfNull(configureOptions);
+            return hostBuilder
+                .ConfigureServices(services =>
+                {
+                    configureOptions.Invoke(services
+                        .AddOptions<MongoDbOptions>(name));
+                });
+        }
+
+        /// <summary>
+        /// Add MongoDb Client Options.
+        /// </summary>
+        /// <param name="hostBuilder">The initial host builder.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="configSectionPath">The config section path.</param>
+        /// <returns>The updated host builder.</returns>
+        public static IHostBuilder AddMongoDbClientOptions(
+            this IHostBuilder hostBuilder,
+            string name,
+            string configSectionPath)
+        {
+            ArgumentNullException.ThrowIfNull(hostBuilder);
+            ArgumentNullException.ThrowIfNull(name);
+            ArgumentNullException.ThrowIfNull(configSectionPath);
+            return hostBuilder
+                .ConfigureServices(services => services
+                    .AddOptions<MongoDbOptions>(name)
+                    .BindConfiguration(configSectionPath));
+        }
+    }
+}
---
+++ src/Client/ServiceCollectionExtensions.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+using Escendit.Orleans.Clients.MongoDb;
+
+/// <summary>
+/// Service Collection Extensions.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Add MongoDb Client Options As Default.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <param name="configureOptions">The configure options.</param>
+    /// <returns>The updated service collection.</returns>
+    public static IServiceCollection AddMongoDbClientOptionsAsDefault(
+        this IServiceCollection services,
+        Action<MongoDbOptions> configureOptions)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(configureOptions);
+        return (IServiceCollection)services
+            .AddOptions<MongoDbOptions>(MongoDbOptions.DefaultKey)
+            .Configure(configureOptions);
+    }
+
+    /// <summary>
+    /// Add MongoDb Client Options.
+    /// </summary>
+    /// <param name="services">The initial service collection.</param>
+    /// <param name="name">The name.</param>
+    /// <param name="configureOptions">The configure options.</param>
+    /// <returns>The updated service collection.</returns>
+    public static IServiceCollection AddMongoDbClientOptions(
+        this IServiceCollection services,
+        string name,
+        Action<MongoDbOptions> configureOptions)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(name);
+        ArgumentNullException.ThrowIfNull(configureOptions);
+        return (IServiceCollection)services
+            .AddOptions<MongoDbOptions>(name)
+            .Configure(configureOptions);
+    }
+}
