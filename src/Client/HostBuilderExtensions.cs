6e48eab11649e0805e5c2180613d3a599c33dfbc
Suhe <sdkaraarslan@gmail.com>
2023-08-24 23:26:09 +0200

    feat: add named registration for mongodb client

--- src/Client/HostBuilderExtensions.cs
+++ src/Client/HostBuilderExtensions.cs
@@ -135,5 +135,166 @@ namespace Escendit.Orleans.Clients.MongoDb
                     .AddOptions<MongoDbOptions>(name)
                     .BindConfiguration(configSectionPath));
         }
+
+        /// <summary>
+        /// Add MongoDb Client As Default.
+        /// </summary>
+        /// <param name="hostBuilder">The initial host builder.</param>
+        /// <param name="configureOptions">The configure options.</param>
+        /// <returns>The updated host builder.</returns>
+        public static IHostBuilder AddMongoDbClientAsDefault(
+            this IHostBuilder hostBuilder,
+            Action<MongoDbOptions> configureOptions)
+        {
+            ArgumentNullException.ThrowIfNull(hostBuilder);
+            ArgumentNullException.ThrowIfNull(MongoDbOptions.DefaultKey);
+            ArgumentNullException.ThrowIfNull(configureOptions);
+            return hostBuilder
+                .AddMongoDbClientOptions(MongoDbOptions.DefaultKey, configureOptions)
+                .ConfigureServices(services =>
+                    services
+                        .AddSingletonNamedService(MongoDbOptions.DefaultKey, MongoDbFactory.CreateMongoDb));
+        }
+
+        /// <summary>
+        /// Add MongoDb Client As Default.
+        /// </summary>
+        /// <param name="hostBuilder">The host builder.</param>
+        /// <param name="configureOptions">The configure options.</param>
+        /// <returns>The updated host builder.</returns>
+        public static IHostBuilder AddMongoDbClientAsDefault(
+            this IHostBuilder hostBuilder,
+            Action<OptionsBuilder<MongoDbOptions>> configureOptions)
+        {
+            ArgumentNullException.ThrowIfNull(hostBuilder);
+            ArgumentNullException.ThrowIfNull(MongoDbOptions.DefaultKey);
+            ArgumentNullException.ThrowIfNull(configureOptions);
+            return hostBuilder
+                .AddMongoDbClientOptions(MongoDbOptions.DefaultKey, configureOptions)
+                .ConfigureServices(services => services
+                    .AddSingletonNamedService(MongoDbOptions.DefaultKey, MongoDbFactory.CreateMongoDb));
+        }
+
+        /// <summary>
+        /// Add MongoDb Client As Default.
+        /// </summary>
+        /// <param name="hostBuilder">The initial host builder.</param>
+        /// <param name="configSectionPath">The config section path.</param>
+        /// <returns>The updated host builder.</returns>
+        public static IHostBuilder AddMongoDbClientAsDefault(
+            this IHostBuilder hostBuilder,
+            string configSectionPath)
+        {
+            ArgumentNullException.ThrowIfNull(hostBuilder);
+            ArgumentNullException.ThrowIfNull(configSectionPath);
+            return hostBuilder
+                .AddMongoDbClientOptions(MongoDbOptions.DefaultKey, configSectionPath)
+                .ConfigureServices(services => services
+                    .AddSingletonNamedService(MongoDbOptions.DefaultKey, MongoDbFactory.CreateMongoDb));
+        }
+
+        /// <summary>
+        /// Add MongoDb Client From Options As Default.
+        /// </summary>
+        /// <remarks>
+        /// It needs provided connection options.
+        /// </remarks>
+        /// <param name="hostBuilder">The initial host builder.</param>
+        /// <param name="optionName">The named option.</param>
+        /// <returns>The updated host builder.</returns>
+        public static IHostBuilder AddMongoDbClientFromOptionAsDefault(
+            this IHostBuilder hostBuilder,
+            string optionName)
+        {
+            return hostBuilder
+                .ConfigureServices(services => services
+                    .AddSingletonNamedService(MongoDbOptions.DefaultKey, (serviceProvider, _) =>
+                        MongoDbFactory.CreateMongoDb(serviceProvider, optionName)));
+        }
+
+        /// <summary>
+        /// Add MongoDb Client.
+        /// </summary>
+        /// <param name="hostBuilder">The initial host builder.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="configureOptions">The configure options.</param>
+        /// <returns>The updated host builder.</returns>
+        public static IHostBuilder AddMongoDbClient(
+            this IHostBuilder hostBuilder,
+            string name,
+            Action<MongoDbOptions> configureOptions)
+        {
+            ArgumentNullException.ThrowIfNull(hostBuilder);
+            ArgumentNullException.ThrowIfNull(name);
+            ArgumentNullException.ThrowIfNull(configureOptions);
+            return hostBuilder
+                .AddMongoDbClientOptions(name, configureOptions)
+                .ConfigureServices(services =>
+                    services
+                        .AddSingletonNamedService(name, MongoDbFactory.CreateMongoDb));
+        }
+
+        /// <summary>
+        /// Add MongoDb Client.
+        /// </summary>
+        /// <param name="hostBuilder">The host builder.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="configureOptions">The configure options.</param>
+        /// <returns>The updated host builder.</returns>
+        public static IHostBuilder AddMongoDbClient(
+            this IHostBuilder hostBuilder,
+            string name,
+            Action<OptionsBuilder<MongoDbOptions>> configureOptions)
+        {
+            ArgumentNullException.ThrowIfNull(hostBuilder);
+            ArgumentNullException.ThrowIfNull(name);
+            ArgumentNullException.ThrowIfNull(configureOptions);
+            return hostBuilder
+                .AddMongoDbClientOptions(name, configureOptions)
+                .ConfigureServices(services => services
+                    .AddSingletonNamedService(name, MongoDbFactory.CreateMongoDb));
+        }
+
+        /// <summary>
+        /// Add MongoDb Client.
+        /// </summary>
+        /// <param name="hostBuilder">The initial host builder.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="configSectionPath">The config section path.</param>
+        /// <returns>The updated host builder.</returns>
+        public static IHostBuilder AddMongoDbClient(
+            this IHostBuilder hostBuilder,
+            string name,
+            string configSectionPath)
+        {
+            ArgumentNullException.ThrowIfNull(hostBuilder);
+            ArgumentNullException.ThrowIfNull(name);
+            ArgumentNullException.ThrowIfNull(configSectionPath);
+            return hostBuilder
+                .AddMongoDbClientOptions(name, configSectionPath)
+                .ConfigureServices(services => services
+                    .AddSingletonNamedService(name, MongoDbFactory.CreateMongoDb));
+        }
+
+        /// <summary>
+        /// Add MongoDb Client From Option.
+        /// </summary>
+        /// <remarks>
+        /// It needs provided connection options.
+        /// </remarks>
+        /// <param name="hostBuilder">The initial host builder.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="optionName">The named option.</param>
+        /// <returns>The updated host builder.</returns>
+        public static IHostBuilder AddMongoDbClientFromOption(
+            this IHostBuilder hostBuilder,
+            string name,
+            string optionName)
+        {
+            return hostBuilder
+                .ConfigureServices(services => services
+                    .AddSingletonNamedService(name, (serviceProvider, _) =>
+                        MongoDbFactory.CreateMongoDb(serviceProvider, optionName)));
+        }
     }
 }
--- src/Client/ServiceCollectionExtensions.cs
+++ src/Client/ServiceCollectionExtensions.cs
@@ -1,49 +1,91 @@
 ﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
 // Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
 
-namespace Microsoft.Extensions.DependencyInjection;
-
-using Escendit.Orleans.Clients.MongoDb;
-
-/// <summary>
-/// Service Collection Extensions.
-/// </summary>
-public static class ServiceCollectionExtensions
+namespace Escendit.Orleans.Clients.MongoDb
 {
-    /// <summary>
-    /// Add MongoDb Client Options As Default.
-    /// </summary>
-    /// <param name="services">The service collection.</param>
-    /// <param name="configureOptions">The configure options.</param>
-    /// <returns>The updated service collection.</returns>
-    public static IServiceCollection AddMongoDbClientOptionsAsDefault(
-        this IServiceCollection services,
-        Action<MongoDbOptions> configureOptions)
-    {
-        ArgumentNullException.ThrowIfNull(services);
-        ArgumentNullException.ThrowIfNull(configureOptions);
-        return (IServiceCollection)services
-            .AddOptions<MongoDbOptions>(MongoDbOptions.DefaultKey)
-            .Configure(configureOptions);
-    }
+    using System.Diagnostics.CodeAnalysis;
+    using Microsoft.Extensions.DependencyInjection;
+    using Orleans.Runtime;
 
     /// <summary>
-    /// Add MongoDb Client Options.
+    /// Service Collection Extensions.
     /// </summary>
-    /// <param name="services">The initial service collection.</param>
-    /// <param name="name">The name.</param>
-    /// <param name="configureOptions">The configure options.</param>
-    /// <returns>The updated service collection.</returns>
-    public static IServiceCollection AddMongoDbClientOptions(
-        this IServiceCollection services,
-        string name,
-        Action<MongoDbOptions> configureOptions)
+    [DynamicallyAccessedMembers(
+        DynamicallyAccessedMemberTypes.All)]
+    public static class ServiceCollectionExtensions
     {
-        ArgumentNullException.ThrowIfNull(services);
-        ArgumentNullException.ThrowIfNull(name);
-        ArgumentNullException.ThrowIfNull(configureOptions);
-        return (IServiceCollection)services
-            .AddOptions<MongoDbOptions>(name)
-            .Configure(configureOptions);
+        /// <summary>
+        /// Add MongoDb Client Options As Default.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        /// <param name="configureOptions">The configure options.</param>
+        /// <returns>The updated service collection.</returns>
+        public static IServiceCollection AddMongoDbClientOptionsAsDefault(
+            this IServiceCollection services,
+            Action<MongoDbOptions> configureOptions)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+            ArgumentNullException.ThrowIfNull(configureOptions);
+            return (IServiceCollection)services
+                .AddOptions<MongoDbOptions>(MongoDbOptions.DefaultKey)
+                .Configure(configureOptions);
+        }
+
+        /// <summary>
+        /// Add MongoDb Client Options.
+        /// </summary>
+        /// <param name="services">The initial service collection.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="configureOptions">The configure options.</param>
+        /// <returns>The updated service collection.</returns>
+        public static IServiceCollection AddMongoDbClientOptions(
+            this IServiceCollection services,
+            string name,
+            Action<MongoDbOptions> configureOptions)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+            ArgumentNullException.ThrowIfNull(name);
+            ArgumentNullException.ThrowIfNull(configureOptions);
+            return (IServiceCollection)services
+                .AddOptions<MongoDbOptions>(name)
+                .Configure(configureOptions);
+        }
+
+        /// <summary>
+        /// Add MongoDb Client as Default.
+        /// </summary>
+        /// <param name="services">The initial service collection.</param>
+        /// <param name="configureOptions">The configure options.</param>
+        /// <returns>The updated service collection.</returns>
+        public static IServiceCollection AddMongoDbClientAsDefault(
+            this IServiceCollection services,
+            Action<MongoDbOptions> configureOptions)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+            ArgumentNullException.ThrowIfNull(configureOptions);
+            return services
+                .AddMongoDbClientOptions(MongoDbOptions.DefaultKey, configureOptions)
+                .AddSingletonNamedService(MongoDbOptions.DefaultKey, MongoDbFactory.CreateMongoDb);
+        }
+
+        /// <summary>
+        /// Add MongoDb Client As Default from Options.
+        /// </summary>
+        /// <param name="services">The initial service collection.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="configureOptions">The configure options.</param>
+        /// <returns>The updated service collection.</returns>
+        public static IServiceCollection AddMongoDbClient(
+            this IServiceCollection services,
+            string name,
+            Action<MongoDbOptions> configureOptions)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+            ArgumentNullException.ThrowIfNull(name);
+            ArgumentNullException.ThrowIfNull(configureOptions);
+            return services
+                .AddMongoDbClientOptions(name, configureOptions)
+                .AddSingletonNamedService(name, MongoDbFactory.CreateMongoDb);
+        }
     }
 }
